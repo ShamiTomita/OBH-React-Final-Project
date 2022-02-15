d14543203e827dae816419b75503e5498c25fc7a
Shami <shamitomita@gmail.com>
2022-02-15 13:09:57 -0500

    added a genres method

--- obh-backend/app/models/content.rb
+++ obh-backend/app/models/content.rb
@@ -1,4 +1,9 @@
 class Content < ApplicationRecord
   has_many :favorited_contents
   has_many :users, :through => :favorited_contents
+
+
+  def genres
+    self.genre.gsub(/,/,"").split(" ")
+  end
 end

25813e14cbe5e75d78116c535fffd370c088c36f
shamitomita <shamitomita@gmail.com>
2023-05-07 22:01:15 -0500

    browser container refactored

--- obh-frontend/src/containers/BrowseContainer.js
+++ obh-frontend/src/containers/BrowseContainer.js
@@ -1,4 +1,4 @@
-import { React, useEffect, } from 'react'
+import { React, useEffect, Fragment } from 'react'
 import {useSelector, useDispatch } from "react-redux"
 import BrowserComponent from '../components/Browse/BrowserComponent'
 import {connect} from 'react-redux'
@@ -13,28 +13,27 @@ const BrowseContainer = () => {
   const dispatch = useDispatch();
   
   useEffect(() => {
-    console.log(content)
     dispatch(clearCurrentShow)
-    console.log()
-  }, [])  
-  let thriller
-  let drama
-  let romance
-  let history
-  let fantasy
-  let adventure
-  let comedy
-  let animation
+  }, [loading])  
+  
   let genres = []
   genres = content.map((media) => media.attributes.genres)
-  console.log(genres)
   let newGenres = [].concat.apply([], genres)
   function onlyUnique(value, index, self) {
    return self.indexOf(value) === index;
     }
   const filteredGenres = newGenres.filter(onlyUnique)
 
-  console.log(filteredGenres)
+  const browserComponents = filteredGenres.map((genre)=>{
+    let genreGroup = content.filter(media => media.attributes.genres.includes(genre))
+    return(
+      <Fragment key={genre+"1"}>
+      <h2 className="movies-shows">{genre}</h2>
+      <BrowserComponent setCurrentShow={setCurrentShow} clearCurrentShow={clearCurrentShow} media={genreGroup} />
+      </Fragment>
+    )
+  })
+ /*  console.log(filteredGenres)
     thriller = content.filter((media) => media.attributes.genres.includes('Thriller'))
     drama = content.filter((media) => media.attributes.genres.includes('Drama'))
     romance = content.filter((media) => media.attributes.genres.includes('Romance'))
@@ -44,25 +43,10 @@ const BrowseContainer = () => {
     comedy = content.filter((media) => media.attributes.genres.includes('Comedy'))
     animation = content.filter((media) => media.attributes.genres.includes('Animation'))
     console.log(thriller, drama, romance, history,)
-
+ */
     return(
       <div className="OBH-Content">
-        <h2 className="movies-shows">Drama</h2>
-        <BrowserComponent setCurrentShow={setCurrentShow} clearCurrentShow={clearCurrentShow} media={drama}/>
-        <h2 className="movies-shows" >Romance</h2>
-        <BrowserComponent setCurrentShow={setCurrentShow} clearCurrentShow={clearCurrentShow} media={romance}/>
-        <h2 className="movies-shows" >Historical</h2>
-        <BrowserComponent setCurrentShow={setCurrentShow} clearCurrentShow={clearCurrentShow} media={history}/>
-        <h2 className="movies-shows">Animation</h2>
-        <BrowserComponent setCurrentShow={setCurrentShow} clearCurrentShow={clearCurrentShow} media={animation}/>
-        <h2 className="movies-shows">Thriller</h2>
-        <BrowserComponent setCurrentShow={setCurrentShow} clearCurrentShow={clearCurrentShow} media={thriller}/>
-        <h2 className="movies-shows">Fantasy</h2>
-        <BrowserComponent setCurrentShow={setCurrentShow} clearCurrentShow={clearCurrentShow} media={fantasy}/>
-        <h2 className="movies-shows">Comedy</h2>
-        <BrowserComponent setCurrentShow={setCurrentShow} clearCurrentShow={clearCurrentShow} media={comedy}/>
-        <h2 className="movies-shows">Adventure</h2>
-        <BrowserComponent setCurrentShow={setCurrentShow} clearCurrentShow={clearCurrentShow} media={adventure}/>
+        {browserComponents}
       </div>
     )
 }
